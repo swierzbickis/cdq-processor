0d8bbbb48bfc8fc07a3d2ea8cef637bac2bc7c20
wierzbickis <s.wierzbicki.kontakt@gmail.com>
2023-01-28 18:04:20 +0100

    Initial commit

---
+++ src/test/java/com/cdq/app/processor/ProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdq.app.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
